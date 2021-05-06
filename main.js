d36d0d5fbad9f88eefef24eb0be45d9364ad7d36
VacuousRom <38498439+VacuousRom@users.noreply.github.com>
2021-05-06 12:21:39 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,14 @@ const functionButtons = document.querySelectorAll('.navbar-item');
 const fileSelector = document.getElementById('file-selector');
 const saveBtn = document.querySelector('.save-JSON');
 const loadBtn = document.querySelector('#btnLoad')
+const loadConfirm = document.querySelector('.load-confirm');
+const cancelBtn = document.querySelector('.cancel-btn')
 let textArea = document.querySelector('.text');
+const warningModal = document.querySelector('.confirmation-modal')
+
+let input = document.getElementById('file-selector')
+let file = input.files[0];
+
 
 // resize for mobile address bar
 let vh = window.innerHeight * 0.01;
@@ -33,7 +40,7 @@ function download() {
     a.href = URL.createObjectURL(new Blob([JSON.stringify(sampleJSON, null, 2)], {
         type: "application/json"
     }));
-    a.setAttribute("download", "plik.JSON");
+    a.setAttribute("download", "textEditor.JSON");
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
@@ -44,6 +51,22 @@ saveBtn.addEventListener('click', download);
 
 // load a JSON file
 
+//check for loaded file
+
+function checkLoaded() {
+    if (!input.files[0]) {
+        alert("Please select a file before clicking 'Load'");
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function setLoadedFile() {
+    input = document.getElementById('file-selector')
+    file = input.files[0];
+}
+
 function loadJSON() {
     let fr;
 
@@ -52,30 +75,37 @@ function loadJSON() {
         return;
     }
 
-    let input = document.getElementById('file-selector')
-    let file = input.files[0];
-    let result;
-
-    if (!input.files[0]) {
-        alert("Please select a file before clicking 'Load'");
-    }
-
-    if (!input.files) {
-        alert("This browser doesn't seem to support the `files` property of file inputs.");
-    }
-
-    fr = new FileReader();
-    fr.onload = receivedText;
-    fr.readAsText(file);
-
-    function receivedText(e) {
-        let lines = e.target.result;
-        var newArr = JSON.parse(lines);
-        textArea.innerHTML = Object.values(newArr);
+    if (checkLoaded()) {
         
+        setLoadedFile();
         
-    }
+        if (!input.files) {
+            alert("This browser doesn't seem to support the `files` property of file inputs.");
+        }
 
+        fr = new FileReader();
+        fr.onload = receivedText;
+        fr.readAsText(file);
+
+        function receivedText(e) {
+            let lines = e.target.result;
+            var newArr = JSON.parse(lines);
+            textArea.innerHTML = Object.values(newArr);
+        }
+        closeModal();
+    }
 }
 
-loadBtn.addEventListener('click', loadJSON)
+loadBtn.addEventListener('click', openModal);
+
+// modal handler
+function openModal() {
+    warningModal.classList.add('modal-in');
+}
+
+function closeModal() {
+    warningModal.classList.remove('modal-in');
+}
+
+loadConfirm.addEventListener('click', loadJSON);
+cancelBtn.addEventListener('click', closeModal);
